ed344ce9e3a774b2ae60bc8aa1f67078961c7d1c
Muhammad Usman <112246337+Usman1361@users.noreply.github.com>
2024-05-03 20:56:54 +0500

    Add Basic Data

---
+++ foodieapp/lib/screens/myhomepage.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:foodieapp/widgets/SayHello.dart';
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SayHello();
+  }
+}
