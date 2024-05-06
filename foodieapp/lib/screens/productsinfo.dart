4baee50f7ac3b4bb58d06e8a2033bdd94fbee198
TalhaNisar <nisartalha99@gmail.com>
2024-05-06 21:50:23 +0500

    updated

--- foodieapp/lib/screens/productsinfo.dart
+++ foodieapp/lib/screens/productsinfo.dart
@@ -20,6 +20,23 @@ class ProductInfoPage extends StatefulWidget {
 }
 
 class _ProductInfoPageState extends State<ProductInfoPage> {
+  double _sliderValue = 0.5;
+
+
+
+ int count = 1;
+
+  void increment() {
+    setState(() {
+      count++;
+    });
+  }
+
+  void decrement() {
+    setState(() {
+      count--;
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -160,19 +177,208 @@ class _ProductInfoPageState extends State<ProductInfoPage> {
               ),
             ),
           ),
-          
           Positioned(
-            top: 610,
+            top: 615,
             left: 0,
-            child: Slider(
-              value: 0,
-              onChanged: (newValue) {},
-              min: 0,
-              max: 1,
-              activeColor: Color(0xFF19C08E),
-              inactiveColor: Colors.grey[300],
+            child: SliderTheme(
+              data: SliderTheme.of(context).copyWith(
+                thumbShape: RoundSliderThumbShape(
+                  enabledThumbRadius: 12.0,
+                ),
+                overlayShape: RoundSliderOverlayShape(
+                  overlayRadius: 20.0,
+                ),
+              ),
+              child: Slider(
+                value: _sliderValue,
+                onChanged: (newValue) {
+                  setState(() {
+                    _sliderValue = newValue;
+                  });
+                },
+                min: 0,
+                max: 1,
+                activeColor: Color(0xFF19C08E),
+                inactiveColor: Colors.grey[300],
+              ),
             ),
           ),
+          Positioned(
+            top: 650,
+            left: 19,
+            child: Container(
+              width: 40,
+              height: 19,
+              child: Text(
+                'Mild',
+                style: TextStyle(
+                  fontFamily: 'Roboto',
+                  fontSize: 12,
+                  fontWeight: FontWeight.w500,
+                  color: Color(0xFF1CC019),
+                ),
+              ),
+            ),
+          ),
+
+          Positioned(
+            top: 650,
+            left: 150,
+            child: Container(
+              width: 40,
+              height: 19,
+              child: Text(
+                'Hot',
+                style: TextStyle(
+                  fontFamily: 'Roboto',
+                  fontSize: 12,
+                  fontWeight: FontWeight.w500,
+                  color: Color(0xFF19C08E),
+                ),
+              ),
+            ),
+          ),
+
+          Positioned(
+            top: 600,
+            left: 260,
+            child: Container(
+              width: 50,
+              height: 19,
+              child: Text(
+                'Portion',
+                style: TextStyle(
+                  fontFamily: 'Roboto',
+                  fontSize: 14,
+                  fontWeight: FontWeight.w500,
+                  color: Color(0xFF3C2F2F),
+                ),
+              ),
+            ),
+          ),
+
+          Positioned(
+          top: 625,
+          left: 260,
+          child: Container(
+            width: 40,
+            height: 40,
+            decoration: BoxDecoration(
+              color: Color(0xFF19C08E),
+              borderRadius: BorderRadius.circular(10),
+              boxShadow: [
+                BoxShadow(
+                  color: Color(0xFF99004F).withOpacity(0.5),
+                  offset: Offset(0, 7),
+                  blurRadius: 13,
+                  spreadRadius: 0,
+                ),
+              ],
+            ),
+            child: InkWell(
+              onTap: decrement,
+              child: Center(
+                child: Text(
+                  "-",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.w900,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+        Positioned(
+          top: 625,
+          left: 335,
+          child: Container(
+            width: 40,
+            height: 40,
+            decoration: BoxDecoration(
+              color: Color(0xFF19C08E),
+              borderRadius: BorderRadius.circular(10),
+              boxShadow: [
+                BoxShadow(
+                  color: Color(0xFF99004F).withOpacity(0.5),
+                  offset: Offset(0, 7),
+                  blurRadius: 13,
+                  spreadRadius: 0,
+                ),
+              ],
+            ),
+            child: InkWell(
+              onTap: increment,
+              child: Center(
+                child: Text(
+                  "+",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.w900,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+        Positioned(
+          top: 625,
+          left: 297,
+          child: Container(
+            width: 40,
+            height: 40,
+           
+            child: Center(
+              child: Text(
+                "$count",
+                style: TextStyle(
+                  color: Color(0xFF3C2F2F),
+                  fontSize: 18,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            ),
+          ),
+        ),
+
+        Positioned(
+  top: 828,
+  left: 19,
+  child: Container(
+    width: 104,
+    height: 70,
+    decoration: BoxDecoration(
+      color: Color(0xFF19C08E),
+      borderRadius: BorderRadius.only(
+        topLeft: Radius.circular(20),
+        bottomLeft: Radius.circular(20),
+      ),
+      boxShadow: [
+        BoxShadow(
+          color: Color(0xFF000000).withOpacity(0.4),
+          offset: Offset(0, 9),
+          blurRadius: 30,
+          spreadRadius: 0,
+        ),
+      ],
+    ),
+    child: Center(
+      child: Text(
+        '\$9.99',
+        style: TextStyle(
+          fontFamily: 'Roboto',
+          fontSize: 22,
+          fontWeight: FontWeight.w600,
+          color: Colors.white,
+        ),
+      ),
+    ),
+  ),
+),
+
+
         ],
       ),
     );
