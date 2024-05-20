63107e644b3205bdff094f737775a3e3e5765485
TalhaNisar <nisartalha99@gmail.com>
2024-05-20 19:51:04 +0500

    changing colors of buttons

--- foodieapp/lib/widgets/framebutton.dart
+++ foodieapp/lib/widgets/framebutton.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:foodieapp/widgets/custombutton.dart';
 
-class FrameWithButtons extends StatelessWidget {
+class FrameWithButtons extends StatefulWidget {
   final Function(String) onCategorySelected; // Callback for category selection
 
   const FrameWithButtons({required this.onCategorySelected});
+
+  @override
+  _FrameWithButtonsState createState() => _FrameWithButtonsState();
+}
+
+class _FrameWithButtonsState extends State<FrameWithButtons> {
+  String selectedCategory = 'All'; // Default selected category
 
   @override
   Widget build(BuildContext context) {
@@ -16,68 +23,82 @@ class FrameWithButtons extends StatelessWidget {
         children: [
           CustomButton(
             text: 'All',
-            color: Color(0xFF19C08E),
-            textColor: Color(0xFFF5F5F5),
-           
+            color: selectedCategory == 'All' ? Color(0xFF19C08E) : Color(0xFFF3F4F6),
+            textColor: selectedCategory == 'All' ? Color(0xFFF5F5F5) : Color(0xFF6A6A6A),
             onTap: () {
-              onCategorySelected('All'); // Call callback with 'All'
+              setState(() {
+                selectedCategory = 'All';
+              });
+              widget.onCategorySelected('All'); // Call callback with 'All'
             },
           ),
           SizedBox(width: 14),
           CustomButton(
             text: 'Burger',
-            color: Color(0xFFF3F4F6),
-            textColor: Color(0xFF6A6A6A),
-            
+            color: selectedCategory == 'Burger' ? Color(0xFF19C08E) : Color(0xFFF3F4F6),
+            textColor: selectedCategory == 'Burger' ? Color(0xFFF5F5F5) : Color(0xFF6A6A6A),
             onTap: () {
-              onCategorySelected('Burger'); // Call callback with 'Burger'
+              setState(() {
+                selectedCategory = 'Burger';
+              });
+              widget.onCategorySelected('Burger'); // Call callback with 'Burger'
             },
           ),
           SizedBox(width: 14),
           CustomButton(
             text: 'Pizza',
-            color: Color(0xFFF3F4F6),
-            textColor: Color(0xFF6A6A6A),
-             
+            color: selectedCategory == 'Pizza' ? Color(0xFF19C08E) : Color(0xFFF3F4F6),
+            textColor: selectedCategory == 'Pizza' ? Color(0xFFF5F5F5) : Color(0xFF6A6A6A),
             onTap: () {
-              onCategorySelected('Pizza'); // Call callback with 'Pizza'
+              setState(() {
+                selectedCategory = 'Pizza';
+              });
+              widget.onCategorySelected('Pizza'); // Call callback with 'Pizza'
             },
           ),
           SizedBox(width: 14),
           CustomButton(
             text: 'Fries',
-            color: Color(0xFFF3F4F6),
-            textColor: Color(0xFF6A6A6A),
-             
+            color: selectedCategory == 'Fries' ? Color(0xFF19C08E) : Color(0xFFF3F4F6),
+            textColor: selectedCategory == 'Fries' ? Color(0xFFF5F5F5) : Color(0xFF6A6A6A),
             onTap: () {
-              onCategorySelected('Fries'); // Call callback with 'Fries'
+              setState(() {
+                selectedCategory = 'Fries';
+              });
+              widget.onCategorySelected('Fries'); // Call callback with 'Fries'
             },
           ),
           CustomButton(
             text: 'Shawarma',
-            color: Color(0xFFF3F4F6),
-            textColor: Color(0xFF6A6A6A),
-             
+            color: selectedCategory == 'Shawarma' ? Color(0xFF19C08E) : Color(0xFFF3F4F6),
+            textColor: selectedCategory == 'Shawarma' ? Color(0xFFF5F5F5) : Color(0xFF6A6A6A),
             onTap: () {
-              onCategorySelected('Shawarma'); // Call callback with 'Shawarma'
+              setState(() {
+                selectedCategory = 'Shawarma';
+              });
+              widget.onCategorySelected('Shawarma'); // Call callback with 'Shawarma'
             },
           ),
           CustomButton(
             text: 'Pasta',
-            color: Color(0xFFF3F4F6),
-            textColor: Color(0xFF6A6A6A),
-             
+            color: selectedCategory == 'Pasta' ? Color(0xFF19C08E) : Color(0xFFF3F4F6),
+            textColor: selectedCategory == 'Pasta' ? Color(0xFFF5F5F5) : Color(0xFF6A6A6A),
             onTap: () {
-              onCategorySelected('Pasta'); // Call callback with 'Pasta'
+              setState(() {
+                selectedCategory = 'Pasta';
+              });
+              widget.onCategorySelected('Pasta'); // Call callback with 'Pasta'
             },
           ),
           CustomButton(
             text: 'Sandwich',
-            color: Color(0xFFF3F4F6),
-            textColor: Color(0xFF6A6A6A),
-            
+            color: selectedCategory == 'Sandwich' ? Color(0xFF19C08E) : Color(0xFFF3F4F6),
+            textColor: selectedCategory == 'Sandwich' ? Color(0xFFF5F5F5) : Color(0xFF6A6A6A),
             onTap: () {
-              onCategorySelected('Sandwich'); // Call callback with 'Sandwich'
+              setState(() {
+                selectedCategory = 'Sandwich';
+              });
+              widget.onCategorySelected('Sandwich'); // Call callback with 'Sandwich'
             },
           ),
         ],
